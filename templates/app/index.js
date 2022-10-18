4ee8f380c937f57b37d5803abe5ad77d4c33f38b
leodev218 <leonardo.dev218@gmail.com>
2022-10-18 18:51:25 -0500

    cambio de estructura

---
+++ templates/app/index.js
@@ -0,0 +1,23 @@
+console.log("Hola");
+
+function add(x1,x2){
+    return x1 + x2;
+}
+function sustract(x1,x2){
+    return x1 - x2;
+}
+function multiply(x1,x2){
+    return x1 * x2;
+}
+function divide(x1,x2){
+    return x1 / x2;
+}
+function saludo(x){
+  x = "Hola leonardo";
+ return x;
+}
+console.log(add(4,2));
+console.log(sustract(8,3));
+console.log(multiply(25,2));
+console.log(divide(50,5));
+console.log(saludo());
